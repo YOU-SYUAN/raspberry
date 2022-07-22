e4a88abe830c05d63cd5a70daabb5a9b5278b214
YOU-SYUAN <vickie890514@gmail.com>
2022-07-22 10:21:53 +0800

    final

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -145,7 +145,7 @@ export default function App() {
   //data.push({ hour: "23", temp: 28, humid: 30 });
   //連接api 抓資料
   function interval() {
-    var dataUrl = "http://192.168.168.113:8080/api/getLast24Hours";
+    var dataUrl = "api/getLast24Hours";
     fetch(dataUrl, { method: "GET" })
       .then((res) => res.json())
       .then((data) => {
@@ -185,7 +185,7 @@ export default function App() {
           label={{
             value: "time",
             position: "insideBottomRight",
-            offset: 2,
+            offset: -15,
             fontSize: "20px",
           }}
         />
@@ -196,6 +196,7 @@ export default function App() {
         <Line
           type="monotone"
           dataKey="temp"
+          unit={"℃"}
           stroke="#82ca9d"
           strokeWidth={3}
           yAxisId={"left"}
@@ -203,6 +204,7 @@ export default function App() {
         <Bar
           type="monotone"
           dataKey="humid"
+          unit={"%"}
           strokeWidth={3}
           fill="#6E8AB6"
           yAxisId={"right"}
